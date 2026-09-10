7ccbc2b62eb81ce045df4df0750bbdbbe47492c5
Arnav Gupta <guptaarnav721@gmail.com>
2026-09-10 23:30:49 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.size(); i++)
+        {
+            minprice = min(minprice, prices[i]);
+
+            int newprofit = prices[i] - minprice;
+
+            profit = max(profit, newprofit);
+        }
+
+        return profit;
+    }
+};
